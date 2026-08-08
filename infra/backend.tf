32bb0b97ae53601b73ddba5b6d38d68745b9e9d5
Yossi Hartman <yossiyhartman@gmail.com>
2026-08-08 18:22:50 +0200

    fix(infra): fix incorrect calling of backend config

---
+++ infra/backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "s3" {
+    # filled by a backend.hcl file
+  }
+}
--- infra/envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "sandbox-snowflake-platform"
-    region       = "eu-north-1"
-    key          = "state/dev/terraform.state"
-    use_lockfile = true
-  }
-}
