ff77e1a064959009c303e43ba885b3dc5671dfa9
Yossi Hartman <yossiyhartman@gmail.com>
2026-08-11 15:28:40 +0200

    style(infra-provider): fixed formatting issue

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,6 +1,3 @@
 provider "snowflake" {
-  preview_features_enabled = [
-    "snowflake_file_formats_datasource",
-    "snowflake_file_format_csv_resource",
-  "snowflake_api_integration_git_repository_github_app"]
+  preview_features_enabled = ["snowflake_file_formats_datasource", "snowflake_file_format_csv_resource", "snowflake_api_integration_git_repository_github_app"]
 }
