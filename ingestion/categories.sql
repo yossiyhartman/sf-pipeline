b05b305281a69eb8a118e20976cc350d6f9f342b
Yossi Hartman <yossiyhartman@gmail.com>
2026-08-11 15:18:49 +0200

    feat(WIP): creating ingestion pattern

---
+++ ingestion/categories.sql
@@ -0,0 +1,20 @@
+create
+or replace table NORTHWIND_DEV.LANDING.CATEGORIES (
+    CATEGORYID INTEGER primary key,
+    CATEGORYNAME VARCHAR(100),
+    DESCRIPTION STRING,
+    PICTURE STRING
+);
+copy into NORTHWIND_DEV.LANDING.CATEGORIES
+from
+(
+    select
+        $1,
+        $2,
+        $3,
+        $4
+    from
+        @NORTHWIND_DEV.LANDING.NORTHWIND_STAGE/categories.csv
+) on_error = abort_statement
+file_format
+= (format_name = NORTHWIND_DEV.LANDING.NORTHWIND_CSV_INGESTION_FORMAT);
